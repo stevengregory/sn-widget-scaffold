8cb18eb3c3204e02d02f9953489ad3a6b68209cc
Steven Gregory <sgregory@gmail.com>
2018-11-16 19:18:21 -0600

    Updated formatting & added prefix var

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ echo "${GREEN}Getting started${RESET_COLOR}"
 
 declare -a args=($@)
 declare -a widgetFolder=()
-for i in "${args[@]}"
-do
+for i in "${args[@]}"; do
   if [[ ${i} != "-a" ]]; then
     widgetFolder+=$(echo -${i} | tr '[:upper:]' '[:lower:]')
   else
@@ -37,16 +36,14 @@ if [[ $1 == *-* ]]; then
   declare -a dashName=()
   IN=$1
   IFS='-' read -ra INPUT <<< "$IN"
-  for i in "${INPUT[@]}"
-  do
+  for i in "${INPUT[@]}"; do
     dashName+="$(tr '[:lower:]' '[:upper:]' <<< ${i:0:1})${i:1}"
   done
   controllerSuffix=$(printf "%s" "${dashName[@]}" && echo "")
 else
   declare -a inputArgs=($@)
   declare -a spaceName=()
-  for i in "${inputArgs[@]}"
-  do
+  for i in "${inputArgs[@]}"; do
     if [[ ${i} != "-a" ]]; then
       spaceName+="$(tr '[:lower:]' '[:upper:]' <<< ${i:0:1})${i:1}"
     fi
@@ -55,7 +52,7 @@ else
 fi
 
 echo "function ${controllerSuffix}Controller() {" > ${PREFIX}${WIDGET}.${CLIENT}
-echo "  var c = this;" >> pe${WIDGET}.${CLIENT}
+echo "  var c = this;" >> ${PREFIX}${WIDGET}.${CLIENT}
 echo "}" >> ${PREFIX}${WIDGET}.${CLIENT}
 touch ${PREFIX}${WIDGET}.${OPTION_SCHEMA}
 curl ${SERVER_GIST} > ${PREFIX}${WIDGET}.${SERVER}
